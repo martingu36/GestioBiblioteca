9fb45e156cca558df5f0c17b6b63b52f844b4bce
space206 <jhostinmco@gmail.com>
2026-05-08 14:12:01 +0200

    Actualizacion de la clase usuario(Metodos:Buscar, Eliminar, enlistar)

--- Usuari.java
+++ Usuari.java
@@ -25,4 +25,35 @@ public class Usuari {
     public void retornarLlibre(Llibre llibre) {
         llibresPrestats.remove(llibre);
     }
+
+    /*Metodos propios de la clase Usuario */
+
+    //Listar usuarios
+    public static void listarUsuaris(List<Usuari> usuaris) {
+        for (Usuari usuari : usuaris) {
+            System.out.println(usuari.getNom());
+        }
+    }
+
+    //Eliminamos el Usuario teniendo en cuenta el nombre del usuario 
+    public static void eliminarUsuari(List<Usuari> usuaris , String nomUsuario){
+      usuaris.removeIf(u -> u.getNom().equalsIgnoreCase(nomUsuario));
+    }
+
+    //Buscar Usuario
+
+    public static void buscarUsuario(List<Usuari> usuaris, String nomUsuario){
+        Boolean encontrado = false;
+        for (Usuari usuari : usuaris) {
+            if (usuari.getNom().equalsIgnoreCase(nomUsuario)) {
+                System.out.println("Usuario encontrado :" +  usuari.toString());
+                encontrado = true;
+                break;
+            }
+        }
+         if (!encontrado) {
+                System.out.println("El usuario" +  nomUsuario + "No existe");
+            } 
+    }
+
 }

d8aa9c70bb5ddb4b2b3675cb3e4b8bccb628d9d1
Mizume25 <gabrielnivicela649@gmail.com>
2026-05-08 14:19:29 +0200

    Contador de LLibre fet

--- Llibre.java
+++ Llibre.java
@@ -5,16 +5,26 @@ public class Llibre {
     private String autor;
     private boolean prestat;
     private String categoria;
+    private int vegadesPrestat;
     
     public Llibre(String titol, String autor, int index) {
         this.titol = titol;
         this.autor = autor;
         this.prestat = false;
         this.categoria = setCategoria(index);
+        this.vegadesPrestat = 0;
     }
-    
+
     public String getCategoria (){
         return this.categoria;
+    }
+
+    public int getVegadesPrestat() {
+        return vegadesPrestat;
+    }
+
+    public void actualitzaContador() {
+        this.vegadesPrestat++;
     }
 
     //Enum de categories 

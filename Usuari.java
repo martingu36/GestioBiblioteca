52661e2884089aa5ce4a2fa9a124104d165d0b85
space206 <70856735+space206@users.noreply.github.com>
2026-05-10 20:54:36 +0200

    Jhostin (#7)
    * Agregar mi nombre al readme y la clase que me toca
    
    * Crear clase de Usuari
    
    * Actualizacion de la clase usuario(Metodos:Buscar, Eliminar, enlistar)
    
    * Historial de Libros prestados por un Usuario
    
    ---------
    
    Co-authored-by: Martin Arriagada <m.arriagada.curihual@gmail.com>

--- Usuari.java
+++ Usuari.java
@@ -4,10 +4,12 @@ import java.util.List;
 public class Usuari {
     private String nom;
     private List<Llibre> llibresPrestats;
+    private List<Prestec> historialPrestecs;
 
     public Usuari(String nom) {
         this.nom = nom;
         this.llibresPrestats = new ArrayList<>();
+        this.historialPrestecs = new ArrayList<>();
     }
 
     public String getNom() {
@@ -25,4 +27,61 @@ public class Usuari {
     public void retornarLlibre(Llibre llibre) {
         llibresPrestats.remove(llibre);
     }
+
+    /*Metodos propios de la clase Usuario */
+
+  
+
+    //Listar usuarios
+    public static void listarUsuaris(List<Usuari> usuaris) {
+        for (Usuari usuari : usuaris) {
+            System.out.println(usuari.getNom());
+        }
+    }
+
+    //Eliminamos el Usuario teniendo en cuenta el nombre del usuario 
+    public static void eliminarUsuari(List<Usuari> usuaris , String nomUsuario){
+      usuaris.removeIf(u -> u.getNom().equalsIgnoreCase(nomUsuario));
+    }
+
+    //Buscar Usuario
+
+    public static void buscarUsuario(List<Usuari> usuaris, String nomUsuario){
+        Boolean encontrado = false;
+        for (Usuari usuari : usuaris) {
+            if (usuari.getNom().equalsIgnoreCase(nomUsuario)) {
+                System.out.println("Usuario encontrado :" +  usuari.toString());
+                encontrado = true;
+                break;
+            }
+        }
+         if (!encontrado) {
+                System.out.println("El usuario" +  nomUsuario + "No existe");
+            } 
+    }
+
+
+      //Hisotrial de libros prestados;
+
+     public void afegirPrestec(Prestec prestec) {
+        historialPrestecs.add(prestec);
+    }
+
+    public void listarHistorial(){
+        System.out.println("Historial de " + nom);
+
+    for (Prestec p : historialPrestecs) {
+
+        System.out.println(
+            "- " +
+            p.getLlibre().getTitol()
+            + " | Data préstec: "
+            + p.getDataPrestec()
+            + " | Retorn: "
+            + p.getDataRetorn()
+        );
+    }
+    }
+
+
 }
